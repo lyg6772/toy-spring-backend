78b30a098a072c46bd16b5a61ca86d56da8f767a
lyg6772 <lyg6772@gmail.com>
2024-06-25 09:48:24 +0900

    test commit

--- src/main/java/com/attendee/toy/model/HelloResponseModel.java
+++ src/main/java/com/attendee/toy/model/HelloResponseModel.java
@@ -4,9 +4,9 @@ public class HelloResponseModel {
 
     private String user_name;
 
-    public String getUser_name() {
-        return user_name;
-    }
+//    public String getUser_name() {
+//        return user_name;
+//    }
 
     public void setUser_name(String name){
         this.user_name = name;
